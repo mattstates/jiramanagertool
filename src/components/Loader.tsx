4a502754caa07b53c2ba02cc0aad8c5155f42ad3
Matt <mattstates@icloud.com>
2019-12-11 08:14:51 -0800

    Loader Formatting

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,5 +2,12 @@ import './Loader.scss';
 import React from 'react';
 
 export const Loader = (): React.ReactElement => {
-    return <div className="loader-ellipsis"><div></div><div></div><div></div><div></div></div>;
+    return (
+        <div className="loader-ellipsis">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
 };
